1fcd083b8e04f0486adc7546731077990595d417
Harry <hg456wilkos@gmail.com>
2026-04-03 18:18:56 +0100

    run once for each file type in session

--- lua/link/init.lua
+++ lua/link/init.lua
@@ -1,14 +1,18 @@
--- lua/link/init.lua
 local M = {}
 function M.setup(opts)
-	require("mason").setup({})
-	require("mason-lspconfig").setup({})
-	require("conform").setup({})
-
-	vim.api.nvim_create_autocmd("BufEnter", {
-		callback = function()
-			require("link.link_class")(opts)
-		end,
-	})
+    require("mason").setup({})
+    require("mason-lspconfig").setup({})
+    require("conform").setup({})
+    local processed_fts = {}
+    vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+            local ft = vim.bo[args.buf].filetype
+            local bt = vim.bo[args.buf].buftype
+            if bt == "" and ft ~= "" and not processed_fts[ft] then
+                processed_fts[ft] = true
+                require("link.link_class")(opts)
+            end
+        end,
+    })
 end
 return M
