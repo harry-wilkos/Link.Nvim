3d20035ca12d877b492cd1a739a33b9c364dfdfb
Harry <hg456wilkos@gmail.com>
2026-04-03 20:27:28 +0100

    file path bug fix

--- lua/link/init.lua
+++ lua/link/init.lua
@@ -1,19 +1,22 @@
 local M = {}
 function M.setup(opts)
-    require("mason").setup({})
-    require("mason-lspconfig").setup({})
-    require("conform").setup({})
-    local processed_fts = {}
-    vim.api.nvim_create_autocmd("FileType", {
-        callback = function(args)
-            local ft = vim.bo[args.buf].filetype
-            local bt = vim.bo[args.buf].buftype
-            local fname = vim.api.nvim_buf_get_name(args.buf)
-            if bt == "" and ft ~= "" and fname ~= "" and not processed_fts[ft] then
-                processed_fts[ft] = true
-                require("link.link_class")(opts)
-            end
-        end,
-    })
+	require("mason").setup({})
+	require("mason-lspconfig").setup({})
+	require("conform").setup({})
+	local processed_fts = {}
+	vim.api.nvim_create_autocmd("FileType", {
+		callback = function(args)
+			local ft = vim.bo[args.buf].filetype
+			if
+				vim.bo[args.buf].buftype == ""
+				and ft ~= ""
+				and vim.api.nvim_buf_get_name(args.buf) ~= ""
+				and not processed_fts[ft]
+			then
+				processed_fts[ft] = true
+				require("link.link_class")(opts)
+			end
+		end,
+	})
 end
 return M
