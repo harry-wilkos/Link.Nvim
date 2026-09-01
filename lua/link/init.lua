7476404e961daafe8b6fc05a40d7e26c3cf2eb74
Harry Wilkinson <hg456wilkos@gmail.com>
2026-09-01 10:45:37 +0100

    deferred startups for quicker loading

--- lua/link/init.lua
+++ lua/link/init.lua
@@ -1,9 +1,10 @@
 local M = {}
+
+local has_initialized_globals = false
+
 function M.setup(opts)
-	require("mason").setup({})
-	require("mason-lspconfig").setup({})
-	require("conform").setup({})
 	local processed_fts = {}
+
 	vim.api.nvim_create_autocmd("FileType", {
 		callback = function(args)
 			local ft = vim.bo[args.buf].filetype
@@ -14,9 +15,18 @@ function M.setup(opts)
 				and not processed_fts[ft]
 			then
 				processed_fts[ft] = true
-				require("link.link_class")(opts)
+				vim.schedule(function()
+					if not has_initialized_globals then
+						require("mason").setup({})
+						require("mason-lspconfig").setup({})
+						require("conform").setup({})
+						has_initialized_globals = true
+					end
+					require("link.link_class")(opts)
+				end)
 			end
 		end,
 	})
 end
+
 return M
